a1750d458c1a66be78da581129d17ddf82d34be0
Someonecursed <93531034+Someonecursed@users.noreply.github.com>
2021-11-02 13:44:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var intro="Hello";
+var inro2="Everyone";
+var joini1i2=intro.concat(" ", inro2);
+console.log(joini1i2);
+console.log("Today we learned about using console in our class, I will be demonstrating some of the things like taking out the sqare of a number, making lowercase to uupercase etc.");
+console.log("the sqare root of 14572-:");
+var squareroot=Math.sqrt(14572);
+console.log(squareroot);
+console.log("The Sum of-:  987607 and 572762")
+var a=987607;
+var b=572762;
+var add = a + b;
+console.log(add);
+console.log("Which Is Greater-: 683545685251 or 64445681333")
+var largest =Math.max(683545685251 , 64445681333)
+console.log(largest);
+console.log("Thank You!!!!!!")
